956e40b5cf9619d918629350427e9ebf964fd076
Destri13 <151584648+Destri13@users.noreply.github.com>
2025-06-01 20:36:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Página de buscador cargada. Google Custom Search Engine debería estar funcionando.");
+    // Aquí podrías añadir cualquier inicialización de JavaScript personalizada si la necesitas.
+});
